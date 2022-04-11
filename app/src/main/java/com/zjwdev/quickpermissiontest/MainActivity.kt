84c5e3cf63a69a75df2a994fbd4a54554b6b33e0
ZJW19890602 <1192935956@qq.com>
2022-04-11 18:40:08 +0800

    提交初始代码

---
+++ app/src/main/java/com/zjwdev/quickpermissiontest/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.zjwdev.quickpermissiontest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.zjwdev.quickpermission.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
