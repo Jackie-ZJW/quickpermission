0a1e36408f4c788716169580cd41e6aa5c5a09a4
ZJW19890602 <1192935956@qq.com>
2022-04-13 18:28:41 +0800

    更新版本号并发布新版本

--- app/src/main/java/com/zjwdev/quickpermissiontest/MainActivity.kt
+++ app/src/main/java/com/zjwdev/quickpermissiontest/MainActivity.kt
@@ -2,7 +2,6 @@ package com.zjwdev.quickpermissiontest
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.zjwdev.quickpermissiontest.R
 
 class MainActivity : AppCompatActivity() {
 
--- app/src/main/java/com/zjwdev/quickpermissiontest/MainFragment.kt
+++ app/src/main/java/com/zjwdev/quickpermissiontest/MainFragment.kt
@@ -1,4 +1,4 @@
-package com.permissionx.app
+package com.zjwdev.quickpermissiontest
 
 import android.Manifest
 import android.graphics.Color
@@ -9,7 +9,6 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.zjwdev.quickpermission.QuickPermission
-import com.zjwdev.quickpermissiontest.CustomDialogFragment
 import com.zjwdev.quickpermissiontest.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
